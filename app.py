cb113ebd288dfdb53e1c97a276075f04b0e05c87
Dani <vateam0864@gmail.com>
2025-11-26 15:12:37 +0800

    first

---
+++ app.py
@@ -0,0 +1,10 @@
+def is_palindrome(s: str) -> bool:
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+if __name__ == "__main__":
+    text = "madam"
+    if is_palindrome(text):
+        print(f"{text} is a palindrome")
+    else:
+        print(f"{text} is NOT a palindrome")
